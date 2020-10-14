b42af33455ea9047b861a782ef6ae8ff2b25282f
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2020-10-14 17:51:36 +0200

    T3EJ4

--- T2EJ1/T2EJ1/EJ4.cs
+++ T2EJ1/T2EJ1/EJ4.cs
@@ -11,5 +11,28 @@ namespace T2EJ1
         private Directivo boss = new Directivo("AAAA","BBBBB",45,"CCCCCC","DDDDDDDD",10);
         private Empleado minion = new Empleado("EEEEEE","FFFFFFFFFFFF",35,"GGGGGG",3500,"111111111");
         private EmpleadoEspecial eliteMinion = new EmpleadoEspecial("HHHHHH","IIIIIIII",32,"JJJJJJJJJJJ",4500,"222222222");
+
+        public void Menu()
+        {
+            int select = 0;
+            switch (select)
+            {
+             case 1:
+
+                    break;
+
+             case 2:
+
+                    break;
+
+             case 3:
+
+                    break;
+
+             case 4:
+
+                    break;
+            }
+        }
     }
 }

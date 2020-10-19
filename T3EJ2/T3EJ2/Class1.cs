0799d64bd58ca22b0dd1f4e65bf566ca8763067a
Matias <louzao.ataria.matias@gmail.com>
2020-10-19 17:44:43 +0200

    comienzo de T3EJ2

---
+++ T3EJ2/T3EJ2/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3EJ2
+{
+    class Aula
+    {
+        private string[] nombres = new string[12];
+        public string[] Nombres
+        {
+            get;
+        }
+
+        public Aula()
+        {
+            for (int i = 0;i < nombres.Length;i++)
+            {
+
+            }
+        }
+
+        public double media(double[,] notas)
+        {
+            double total = 0;
+            for (int i = 0;i < nombres.Length;i++)
+            {
+                for (int j = 0;j < notas.Length;j++)
+                {
+                    total += notas[j, 0];
+                }
+            }
+            return total/(nombres.Length*notas.Length);
+        }
+
+
+    }
+}
---
+++ T3EJ2/T3EJ2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3EJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aula a = new Aula();//FIXME todo en general
+            Alumno a2 = new Alumno(a.Nombres.Length);
+            Console.WriteLine(a.media(a2.Notas));
+        }
+    }
+
+    enum Subjects
+    {
+        Mathematics = 1,
+        Physics = 2,
+        Technical_drawing = 3,
+        Algebra = 4
+    }
+}

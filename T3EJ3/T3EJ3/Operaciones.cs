6e026a742bee7d6c092efef4d5a7a2a5e557ee73
Matias <louzao.ataria.matias@gmail.com>
2020-10-28 13:50:04 +0100

    comienzo de T3EJ3

---
+++ T3EJ3/T3EJ3/Operaciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3EJ3
+{
+    class Operaciones
+    {
+        private ArrayList juegos = new ArrayList();
+    }
+}
---
+++ T3EJ3/T3EJ3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3EJ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+struct Juego
+{
+    String titulo;
+    int año;
+    Estilo est;
+}
+enum Estilo{
+    Arcade = 0,
+    Videoaventura = 1,
+    Shootemup = 2,
+    Estrategia = 3,
+    Deportivo = 4
+}

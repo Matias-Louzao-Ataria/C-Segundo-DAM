97777ad5704ff0010266d06040daff1e8569880d
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2020-11-04 23:07:01 +0100

    Servicios EJ1

---
+++ MyTaskManager/MyTaskManager/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MyTaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ViewProcesses = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(707, 347);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(261, 408);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // ViewProcesses
+            // 
+            this.ViewProcesses.Location = new System.Drawing.Point(166, 12);
+            this.ViewProcesses.Name = "ViewProcesses";
+            this.ViewProcesses.Size = new System.Drawing.Size(75, 23);
+            this.ViewProcesses.TabIndex = 2;
+            this.ViewProcesses.Text = "ViewProcesses";
+            this.ViewProcesses.UseVisualStyleBackColor = true;
+            this.ViewProcesses.Click += new System.EventHandler(this.ViewProcesses_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(409, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(490, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ViewProcesses);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button ViewProcesses;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ MyTaskManager/MyTaskManager/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MyTaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewProcesses_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            this.textBox1.Text += "Nombre de proceso | Process Name | PID\r\n";
+            foreach (Process p in processes)
+            {
+                String pMain = p.MainWindowTitle,pName = p.ProcessName,pid = ""+p.Id;
+
+                if (pMain.Length < 18)
+                {
+                    for (int i = 0; i < 18 - pMain.Length; i++)
+                    {
+                        pMain += " ";
+                    }
+                }
+                
+                if (pName.Length < 12)
+                {
+                    for (int i = 0;i < 13-pName.Length;i++)
+                    {
+                        pName += " ";
+                    }
+                }
+
+                if (pid.Length < 3)
+                {
+                    for (int i = 0;i < 4-pid.Length;i++)
+                    {
+                        pid += " ";
+                    }
+                }
+
+                string algo = string.Format("{0}  | {1} | {2}\r\n",p.MainWindowTitle.Length > 0 ? p.MainWindowTitle.Length > 18 ? p.MainWindowTitle.Substring(0, 18) + "...:" : p.MainWindowTitle+":" : pMain, p.ProcessName.Length > 0 ? p.ProcessName.Length > 8 ? p.ProcessName.Substring(0, 8) + "...:" : pName + ":" : "", pid);
+                this.textBox1.Text += algo;
+            }
+        }
+    }
+}

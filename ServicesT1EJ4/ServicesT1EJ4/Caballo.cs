84243f4c936316da82cc47be68f516107484f4d8
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2020-11-12 23:29:14 +0100

    Avances en Servicios EJ4

--- ServicesT1EJ4/ServicesT1EJ4/Caballo.cs
+++ ServicesT1EJ4/ServicesT1EJ4/Caballo.cs
@@ -9,11 +9,9 @@ namespace ServicesT1EJ4
 {
     class Caballo
     {
-        public static readonly Object l = new object();
+        
         public static bool running = true;
-        private int id;
         public int Id { set; get;}
-
         private int x = 0;
         private int conx, cony;
 
@@ -86,38 +84,45 @@ namespace ServicesT1EJ4
 
         public void correr()//Existe la posibilidad de que 2 llegen al mismo tiempo.
         {
+            Random r = new Random();
             Console.CursorVisible = false;
             while (running)
             {
-                this.X++;
-                lock (l)
+                lock (Program.l)
                 {
-                    if (x >= 8)//Puede que esto arregle la llegada simultanea.
+                    this.X += r.Next(0,4); 
+                    if (x >= 12)//Puede que esto arregle la llegada simultanea.
                     {
                         running = false;
                     }
                 }
-                for (int i = 0; i <= this.X; i++)
-                {
-                    lock (l)//IMPORTANTE todo lo que haga cosas con this. (excepto comparaciones) fuera del lock porque es especifico de un objeto y no es un recurso compartido.
+                if(running)   
+                Thread.Sleep(r.Next(0,500));
+            }
+            for (int i = 0; i <= this.X; i++)
+            {
+                lock (Program.l)//IMPORTANTE todo lo que haga cosas con this. (excepto comparaciones) fuera del lock porque es especifico de un objeto y no es un recurso compartido.
                         //Hago esto porque lock es para evitar que un hilo acceda a datos que está usando otro (recursos compartidos), en este caso la consola.
+                {
+                    Console.SetCursorPosition(this.Conx, this.Cony);
+                    if (this.X == i)
                     {
-                        Console.SetCursorPosition(this.Conx, this.Cony);
-                        if (this.X == i)
-                        {
-                            Console.WriteLine("#");
-                        }
-                        else
-                        {
-                            Console.Write("_");
-                        }
+                        Console.WriteLine("#");
                     }
-                    if (this.conx < Console.BufferWidth-1)
+                    else
                     {
-                        this.conx++;
+                        Console.Write("_");
                     }
-                    Thread.Sleep(50);
                 }
+                if (this.conx < Console.BufferWidth - 1)
+                {
+                    this.conx++;
+                }
+            }
+            if (this.x >= 12)
+            {
+                lock (Program.l)
+                    Monitor.Pulse(Program.l);
             }
         }
     }
--- ServicesT1EJ4/ServicesT1EJ4/Program.cs
+++ ServicesT1EJ4/ServicesT1EJ4/Program.cs
@@ -9,23 +9,97 @@ namespace ServicesT1EJ4
 {
     class Program
     {
+        public static readonly Object lmain = new object();
+        public static readonly Object l = new object();
         static void Main(string[] args)
         {
             Caballo[] runners = new Caballo[5];
+            bool error = false;
+            bool replay = false;
 
-            for (int i = 0;i < runners.Length ;i++)
+            do
             {
-                runners[i] = new Caballo(i,0,i+1);
-            }
+                Console.WriteLine("Enter the horse you want to bet for: 1 for the first horse and {0} for the last one!", runners.Length);
+                int select = AskForInteger();
+                select--;
+                if (select >= 0 && select < runners.Length)
+                {
+                    for (int i = 0; i < runners.Length; i++)
+                    {
+                        runners[i] = new Caballo(i, 0, i + 1);
+                    }
 
-            Thread[] threads = new Thread[runners.Length];
+                    Thread[] threads = new Thread[runners.Length];
 
-            for (int i = 0;i < threads.Length ;i++)
-            {
-                threads[i] = new Thread(runners[i].correr);
-                threads[i].Start();
-            }
+                    for (int i = 0; i < threads.Length; i++)
+                    {
+                        threads[i] = new Thread(runners[i].correr);
+                        threads[i].Start();
+                    }
+
+                    //threads[0].Join();
+                    while (Caballo.running)
+                    {
+                        lock (l)
+                        {
+                            Monitor.Wait(l);
+                        }
+                    }
+                    for (int i = runners.Length - 1; i >= 0; i--)
+                    {
+                        lock (l)
+                        {
+                            if (runners[i].X >= 12)
+                            {
+                                Console.SetCursorPosition(0, 10);
+                                Console.WriteLine(runners[i].Id+1);
+                                if (select == runners[i].Id)
+                                {
+                                    Console.WriteLine("You win!");
+                                    Console.WriteLine("Would you like to play again?");
+                                    if (Console.ReadLine().Contains("yes"))
+                                    {
+                                        replay = true;
+                                    }
+                                    else
+                                    {
+                                        replay = false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    error = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid horse!");
+                    error = true;
+                }
+            } while (error ||replay);
+            
             Console.ReadKey();
+        }
+
+        public static int AskForInteger()
+        {
+            bool error = true;
+            int res = 0;
+            while (error)
+            {
+                try
+                {
+                    res = int.Parse(Console.ReadLine());
+                    error = false;
+                }
+                catch (Exception e) when (e is FormatException || e is OverflowException)
+                {
+                    Console.WriteLine("Invalid number!");
+                    Console.WriteLine("Please enter a new one:");
+                    error = true;
+                }
+            }
+            return res;
         }
     }
 }

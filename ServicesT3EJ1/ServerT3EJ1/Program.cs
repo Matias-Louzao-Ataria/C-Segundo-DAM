f98c8a3e3bc81eceaebd1fa08617ce0b96b881e7
Matias <louzao.ataria.matias@gmail.com>
2021-01-11 17:41:15 +0100

    Comienzo Services T3EJ1

---
+++ ServicesT3EJ1/ServerT3EJ1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ServerT3EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any,2609);
+            Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            socket.Bind(endPoint);
+            socket.Listen(1);
+            Socket client = socket.Accept();
+            using (NetworkStream ns = new NetworkStream(client))
+            using (StreamReader reader = new StreamReader(ns))
+            {
+                using (StreamWriter writer = new StreamWriter(ns))
+                {
+                    writer.WriteLine("Hey from socket!");
+                    writer.Flush();
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+            client.Close();
+            socket.Close();
+            Console.ReadKey();
+        }
+    }
+}

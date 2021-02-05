aefd2ae7a5ebde725ad448bc87e7f49fc2615824
Matias <louzao.ataria.matias@gmail.com>
2021-02-05 12:10:18 +0100

    Empieza DIT5EJ1

---
+++ DIT5EJ1/DIT5EJ1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DIT5EJ1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTextbox1 = new DIT5EJ1.LabelTextbox();
+            this.SuspendLayout();
+            // 
+            // labelTextbox1
+            // 
+            this.labelTextbox1.Location = new System.Drawing.Point(234, 114);
+            this.labelTextbox1.Name = "labelTextbox1";
+            this.labelTextbox1.Posicion = DIT5EJ1.LabelTextbox.ePosicion.IZQUIERDA;
+            this.labelTextbox1.Separacion = 0;
+            this.labelTextbox1.Size = new System.Drawing.Size(154, 20);
+            this.labelTextbox1.TabIndex = 0;
+            this.labelTextbox1.TextLbl = "label1";
+            this.labelTextbox1.TextTxt = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTextbox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LabelTextbox labelTextbox1;
+    }
+}
---
+++ DIT5EJ1/DIT5EJ1/LabelTextbox.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIT5EJ1
+{
+
+    [
+        DefaultProperty("TextLbl"),
+        DefaultEvent("Load")
+    ]
+
+    public partial class LabelTextbox : UserControl
+    {
+        public enum ePosicion
+        {
+            IZQUIERDA, DERECHA
+        }
+
+        private ePosicion posicion = ePosicion.IZQUIERDA;
+
+        [Category("Appearance")]
+        [Description("Indica si la Label se sitúa a la IZQUIERDA o DERECHA del Textbox")]
+        public ePosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(ePosicion), value))
+                {
+                    posicion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return posicion;
+            }
+        }
+
+        private int separacion = 0;
+        [Category("Design")]
+        [Description("Píxels de separación entre Label y Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return separacion;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado a la Label del control")]
+        public string TextLbl
+        {
+            set
+            {
+                lbl.Text = value;
+                recolocar();
+            }
+            get
+            {
+                return lbl.Text;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado al TextBox del control")]
+        public string TextTxt
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        public LabelTextbox()
+        {
+            InitializeComponent();
+            TextLbl = Name;
+            TextTxt = "";
+            recolocar();
+        }
+
+        private void recolocar()
+        {
+            switch (posicion)
+            {
+                case ePosicion.DERECHA:
+                    //Establecemos posición del componente txt
+                    txt.Location = new Point(0, 0);
+                    //Establecemos ancho del Textbox (la label tiene ancho fijo)
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(txt.Width + Separacion, 0);
+                    //Establecemos altura del componente
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+                case ePosicion.IZQUIERDA:
+                    lbl.Location = new Point(0, 0);
+                    txt.Location = new Point(lbl.Width + Separacion, 0);
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+            }
+        }
+        // Esta función has de enlazarla con el evento SizeChanged.
+        // Sería necesario también tener en cuenta otros eventos como FontChanged
+        // que aquí nos saltamos.
+        private void LabelTextBox_SizeChanged(object sender, EventArgs e)
+        {
+            recolocar();
+        }
+    }
+}
